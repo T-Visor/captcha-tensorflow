46cfa658a81c05ab4541d4abcbed8102b3dbe32c
T-Visor <turkimbro@yahoo.com>
2021-09-08 14:14:44 -0400

    created different notebooks for several CAPTCHA scenarios

--- alternate-generator.py
+++ alternate-generator.py
@@ -3,9 +3,11 @@
 import captcha
 from captcha.image import ImageCaptcha
 
-FONTS =['/usr/share/fonts/TTF/System San Francisco Display Regular.ttf',
-        '/usr/share/fonts/TTF/MesloLGS-NF-Bold-Italic.ttf',
-        '/usr/share/fonts/TTF/DejaVuSerif.ttf']
+#FONTS =['/usr/share/fonts/TTF/System San Francisco Display Regular.ttf',
+#        '/usr/share/fonts/TTF/MesloLGS-NF-Bold-Italic.ttf',
+#        '/usr/share/fonts/TTF/DejaVuSerif.ttf']
+
+DESTINATION = 'datasets/complex/'
 
 
 
@@ -15,7 +17,7 @@ for number in range(0, 10000):
     captcha_text = str(number)
     captcha_text = captcha_text.zfill(4)
 
-    # Create and save the CAPTCHA image.
-    image = ImageCaptcha(width = 80, height = 60, fonts=FONTS)
+    image = ImageCaptcha(width = 120, height = 100) 
+
     data = image.generate(captcha_text)  
-    image.write(captcha_text, 'datasets/training/' + captcha_text + '_alt.png')
+    image.write(captcha_text, DESTINATION + captcha_text + '_image.png')
